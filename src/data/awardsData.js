8fc6899551e083e7bfcadc274ee9756ab66410d4
Sudharshan S <93808025+Sudharshan281@users.noreply.github.com>
2023-06-25 08:28:19 +0530

    Add files via upload

---
+++ src/data/awardsData.js
@@ -0,0 +1,27 @@
+import image1 from "../images/a1.png";
+import image2 from "../images/a2.png";
+import image3 from "../images/a3.png";
+
+export const awardsData = [
+  {
+    image: image1,
+    review:
+      "Our Digital Wizards team won one gold and two bronze medals in Inter IIT Tech Meet 11.0. This encompasses all the ML problem statements released this year.",
+    name: "TEAMA",
+    status: "TECH INTERIIT",
+  },
+  {
+    image: image2,
+    review:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi ipsam, ab itaque nam perferendis impedit sint ",
+    name: "TEAMB",
+    status: "AUTOMOBILE",
+  },
+  {
+    image: image3,
+    review:
+      " Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima aspernatur quod voluptatem",
+    name: "TEAMC",
+    status: "DESIGN",
+  },
+];
