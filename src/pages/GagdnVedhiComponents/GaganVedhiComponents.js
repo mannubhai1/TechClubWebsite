7550f5fac1005b130280c7102891e49d72af1a81
yash arya <cs21b057@iittp.ac.in>
2023-06-26 10:45:38 +0530

    new

--- src/pages/GagdnVedhiComponents/GaganVedhiComponents.js
+++ src/pages/GagdnVedhiComponents/GaganVedhiComponents.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "./GaganVedhiComponents.module.css";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
 function GaganVedhiComponents() {
   const email = "astronomyclub@iittp.ac.in";
