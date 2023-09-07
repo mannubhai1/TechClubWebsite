e5376971f12ce85d274ec0ef25660c7361ea886b
Manurbhav Arya <cs21b057@iittp.ac.in>
2023-09-07 13:45:00 +0530

    new changes

--- src/pages/GagdnVedhiComponents/Activities.js
+++ src/pages/GagdnVedhiComponents/Activities.js
@@ -1,129 +1,125 @@
-import React from 'react'
-import styles from './Activities.module.css'
-import { useEffect } from 'react';
+import React from "react";
+import styles from "./Activities.module.css";
+import { useEffect } from "react";
+
+// activity card to display recent and upcoming events
 
 const Activities = () => {
-    var hidden1;
-    var hidden2; 
-    useEffect(() => {
-        hidden1 = document.querySelector('.curr');
-        hidden2 = document.querySelector('.upcoming');
-    }, [])
-    
+  var hidden1;
+  var hidden2;
 
-    const handleClick1 = ()=> {
-        // console.log(hidden1 + " hey ");
-        if(hidden1!==null && hidden1.classList.contains('hidden')){
-            hidden1.classList.remove('hidden');
-            hidden1.classList.add('flex');
-            hidden2.classList.remove('flex');
-            hidden2.classList.add('hidden');
-        }
-       
+  useEffect(() => {
+    hidden1 = document.querySelector(".recent");        // to get the element with class name 'recent'
+    hidden2 = document.querySelector(".upcoming");      // to get the element with class name 'upcoming'
+  }, []);
+
+  const handleClick1 = () => {
+    if (hidden1 !== null && hidden1.classList.contains("hidden")) {       // to check if the element is hidden or not
+      hidden1.classList.remove("hidden");
+      hidden1.classList.add("flex");
+      hidden2.classList.remove("flex");
+      hidden2.classList.add("hidden");
     }
-    const handleClick2 = ()=> {
-        if(hidden2!==null && hidden2.classList.contains('hidden')){
-            hidden2.classList.remove('hidden');
-            hidden2.classList.add('flex');
-            hidden1.classList.remove('flex');
-            hidden1.classList.add('hidden');
-        }
+  };
+  const handleClick2 = () => {
+    if (hidden2 !== null && hidden2.classList.contains("hidden")) {       // to check if the element is hidden or not
+      hidden2.classList.remove("hidden");
+      hidden2.classList.add("flex");
+      hidden1.classList.remove("flex");
+      hidden1.classList.add("hidden");
     }
+  };
 
-    return (
-        <>
-            <main>
-
-            <section id={styles.mainCard}>
-
-                <div>
-                    <img id={styles.profilePic} src="./gv_image/gv.jpg" alt="GV"/>
-                        <p className='text-xl'>Events of</p>
-                        <h1 className='text-5xl'>Gagan Vedhi</h1>
-                </div>
-                <fieldset>
-                    <div className={`flex flex-col md:flex-wrap p-0 h-[25vh]`}>
-                        <div className={styles.eventDiv}>
-                            <button onClick={handleClick1} className={`px-[4.8vh] py-4 font-sans text-xl font-bold `}> <h3 className={styles.eventheading}>Recent Events</h3></button>
-                        </div>
-                        <div className={styles.eventDiv}>
-                            <button onClick={handleClick2} className={`px-5 py-4 m-auto font-sans text-xl font-bold`}><h3 className={styles.eventheading}>Upcoming Events</h3></button>
-                        </div>
-                    </div>
-                </fieldset>
-
-            </section>
-            <div className='flex flex-col gap-8 ml-8 w-[72vw]'> 
-
-                <div className={`curr flex flex-row gap-4 ${styles.eventView}`}>
-
-                    <div className={`${styles.activityContainer}`}>
-                        <h2>Regional Science Centre Trip</h2> 
-
-                        <p >A three-hour trip to RSC of Tirupati for a sky-gazing session.</p>
-                        {/* <span>dd/mm/yyyy</span> */}
-
-                    </div>
-
-                    <div className={`${styles.activityContainer}`}>
-                        <h2>Athereum</h2>
-
-                        <p>A Joint fest organized by Astronomy Clubs of IIT and IISER Tirupati in which many competitions were conducted.</p>
-                        {/* <span>dd/mm/yyyy</span> */}
-
-                    </div>
-
-                    <div className={`${styles.activityContainer}`}>
-                        <h2>LIGO Workshop</h2>
-                        <p>A two-day Offline workshop on Gravitational Waves Detection by Dr. Apratim and Dr. Suresh from IUCAA Pune. It had received huge participation from IIT and IISER Tirupati. The first day was held at IISER, and the second day at IIT.</p>
-                        {/* <span>dd/mm/yyyy</span> */}
-
-                    </div>
-
-                </div>
-
-                <div className={`upcoming hidden flex-row gap-4 ${styles.eventView}`}>
-
-                    <div className={`${styles.activityContainer2}`}>
-
-                        <div className={styles.upcomingEvents}>
-                            <h3 className={`text-white px-2`}>Documentary cum quiz 3.0</h3>
-                        </div>
-                        <div className={styles.upcomingEvents}>
-                            <h3 className={`text-white px-2`}>Movie night</h3>
-                        </div>
-                        <div className={styles.upcomingEvents}>
-                            <h3 className={`text-white px-2`}>ML Workshop series</h3>
-                        </div>
-                        <div className={styles.upcomingEvents}>
-                            <h3 className={`text-white px-2`}>A working rocket model(Project)</h3>
-                        </div>
-                        <div className={styles.upcomingEvents}>
-                            <h3 className={`text-white px-2`}>StarGazing (For some special events)</h3>
-                        </div>
-
-
-                    </div>
-
-                    {/* <div className={`${styles.activityContainer} `}>
-                        <h2>Movie night</h2>
-
-                        <p>Stay tuned</p>
-
-                    </div> */}
-
-                    {/* <div className={`${styles.activityContainer}`}>
-                        <h2>Event 6</h2>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde qui fugiat sapiente, iste adipisci architecto eius quidem cumque officia optio dolorum. Nemo vel optio voluptas maiores hic quasi, minus distinctio!</p>
-
-                    </div> */}
-
-                </div>
-
+  return (
+    <>
+      <main>
+        <section id={styles.mainCard}>
+          <div>
+            <img id={styles.profilePic} src="./gv_image/gv.jpg" alt="GV" />
+            <p className="text-xl">Events of</p>
+            <h1 className="text-5xl">Gagan Vedhi</h1>
+          </div>
+          <fieldset>
+            <div className={`flex flex-col md:flex-wrap p-0 h-[25vh]`}>
+              <div className={styles.eventDiv}>
+                <button
+                  onClick={handleClick1}
+                  className={`px-[4.8vh] py-4 font-sans text-xl font-bold `}
+                >
+                  {" "}
+                  <h3 className={styles.eventheading}>Recent Events</h3>
+                </button>
+              </div>
+              <div className={styles.eventDiv}>
+                <button
+                  onClick={handleClick2}
+                  className={`px-5 py-4 m-auto font-sans text-xl font-bold`}
+                >
+                  <h3 className={styles.eventheading}>Upcoming Events</h3>
+                </button>
+              </div>
             </div>
-        </main>
-    </>
-    )
-}
+          </fieldset>
+        </section>
+        <div className="flex flex-col gap-8 ml-8 w-[72vw]">
+          <div className={`recent flex flex-row gap-4 ${styles.eventView}`}>
+            {" "}
+                                                                              {/* to display recent events */}
+            <div className={`${styles.activityContainer}`}>
+              <h2>Regional Science Centre Trip</h2>
 
-export default Activities
+              <p>
+                A three-hour trip to RSC of Tirupati for a sky-gazing session.
+              </p>
+            </div>
+            <div className={`${styles.activityContainer}`}>
+              <h2>Athereum</h2>
+
+              <p>
+                A Joint fest organized by Astronomy Clubs of IIT and IISER
+                Tirupati in which many competitions were conducted.
+              </p>
+            </div>
+            <div className={`${styles.activityContainer}`}>
+              <h2>LIGO Workshop</h2>
+              <p>
+                A two-day Offline workshop on Gravitational Waves Detection by
+                Dr. Apratim and Dr. Suresh from IUCAA Pune. It had received huge
+                participation from IIT and IISER Tirupati. The first day was
+                held at IISER, and the second day at IIT.
+              </p>
+            </div>
+          </div>
+
+          <div className={`upcoming hidden flex-row gap-4 ${styles.eventView}`}>
+            {" "}
+                                                                                      {/* to display upcoming events */}
+            <div className={`${styles.activityContainer2}`}>
+              <div className={styles.upcomingEvents}>
+                <h3 className={`text-white px-2`}>Documentary cum quiz 3.0</h3>
+              </div>
+              <div className={styles.upcomingEvents}>
+                <h3 className={`text-white px-2`}>Movie night</h3>
+              </div>
+              <div className={styles.upcomingEvents}>
+                <h3 className={`text-white px-2`}>ML Workshop series</h3>
+              </div>
+              <div className={styles.upcomingEvents}>
+                <h3 className={`text-white px-2`}>
+                  A working rocket model(Project)
+                </h3>
+              </div>
+              <div className={styles.upcomingEvents}>
+                <h3 className={`text-white px-2`}>
+                  StarGazing (For some special events)
+                </h3>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default Activities;
--- src/pages/GagdnVedhiComponents/GaganVedhiComponents.js
+++ src/pages/GagdnVedhiComponents/GaganVedhiComponents.js
@@ -3,7 +3,7 @@ import styles from "./GaganVedhiComponents.module.css";
 import Activities from "./Activities";
 import { motion } from "framer-motion";
 
-function GaganVedhiComponents() {
+function GaganVedhiComponents() {             //constants for links
   const email = "astronomyclub@iittp.ac.in";
   const discord = "https://discord.com/invite/7nqZBQX9";
   const linkedin = "https://www.linkedin.com/in/gagan-vedhi-iitt-129314244/";
@@ -48,6 +48,7 @@ function GaganVedhiComponents() {
 
         <div className={styles.desc}>
           <div className={`${styles.box}`}>
+
             <div className={`${styles.titleContainer} ${styles.gazingcontainer}`}>
               <div className={styles.titleWords}>
                 <div className={`${styles.title1} ${styles.gazingtitle1}`}>
@@ -57,7 +58,8 @@ function GaganVedhiComponents() {
                 </div>
               </div>
             </div>
-            <p className="italic text-black object-contain rounded-2xl font-semibold bold h-[75vh] w-[45vw] overflow-auto px-10 color-white bg-white py-8 my-16 ">
+
+            <p className="italic text-black object-contain rounded`-2xl font-medium bold h-[75vh] w-[45vw] overflow-auto px-10 color-white bg-white py-8 my-16 ">
               Exploring the vast expanse of the universe and uncovering its
               secrets is a thrilling experience right?, and it becomes even more
               exciting when shared with like-minded individuals. That's what
@@ -75,6 +77,8 @@ function GaganVedhiComponents() {
         </div>
         <br />
         <br />
+
+
 
 
 
@@ -96,10 +100,9 @@ function GaganVedhiComponents() {
             </div>
           </div>
           <div className={`${styles.aboutcont} flex flex-row justify-around content-around gap-8`}>
-            {/* <div className=" col-md-5 md:m-5 md:rounded-3xl md:border-2 border-cyan-500 overflow-auto bg-white"> */}
             <div className="flex flex-col md:ml-[10vh] md:rounded-3xl md:border-2 max-w-2xl border-cyan-500 w-1/2 overflow-auto bg-white">
               <h2 className="flex flex-col justify-center m-auto font-bold py-3 justify-self-center">History</h2>
-              <p className=" text-black italic font-semibold md:text-lg bold text-left md:px-8 md:py-3 overflow-auto md:tracking-wide color-white">
+              <p className=" text-black italic font-medium md:text-lg bold text-left md:px-8 md:py-3 overflow-auto md:tracking-wide color-white">
                 Gagan Vedhi, is a community of astronomy enthusiasts who are
                 passionate about exploring the mysteries of the universe. The
                 club was founded 6 years ago by Anup Kulkarni with the name
@@ -115,7 +118,7 @@ function GaganVedhiComponents() {
             </div>
             <div className="flex flex-col w-1/2 md:mr-[10vh] md:rounded-3xl md:border-2 border-cyan-500 overflow-auto bg-white">
               <h2 className="color-yellow font-bold mt-2 md:py-3">Motivation</h2>
-              <p className="text-black text-left font-bold italic px-12 py-8 tracking-widest md:text-xl">
+              <p className="text-black text-left font-medium italic px-12 py-8 tracking-widest md:text-xl">
                 The club's motivation is to create a platform for students where
                 they can come together to explore the mysteries of the universe.
                 The Club Aims to establish a strong community of Astrophiles to
@@ -276,11 +279,17 @@ function GaganVedhiComponents() {
           <br />
 
 
+
+
           <Activities/> 
 
 
 
+
+
           {/* Team */}
+
+
 
 
 
