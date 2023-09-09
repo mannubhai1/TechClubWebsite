0bdab1b349bb88a6cf29af9e864dd0f3caec29bd
Ashish kumar R <95536946+ashroaban003@users.noreply.github.com>
2023-09-09 14:28:32 +0530

    Add files via upload

---
+++ src/data/dwprevData.js
@@ -0,0 +1,58 @@
+export const dwprevData = [
+   
+    {
+      icon: (
+        <svg
+          width="28"
+          height="24"
+          viewBox="0 0 28 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path d="M3.11111 18L0 1.5L8.55556 9L14 0L19.4444 9L28 1.5L24.8889 18H3.11111ZM24.8889 22.5C24.8889 23.4 24.2667 24 23.3333 24H4.66667C3.73333 24 3.11111 23.4 3.11111 22.5V21H24.8889V22.5Z" />
+        </svg>
+      ),
+      name: "DSA series",
+      features: [
+        "practice session",
+        "solution session",
+        "lorep ipsum",
+      ],
+    },
+    {
+        icon: (
+          <svg
+            width="28"
+            height="24"
+            viewBox="0 0 28 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M3.11111 18L0 1.5L8.55556 9L14 0L19.4444 9L28 1.5L24.8889 18H3.11111ZM24.8889 22.5C24.8889 23.4 24.2667 24 23.3333 24H4.66667C3.73333 24 3.11111 23.4 3.11111 22.5V21H24.8889V22.5Z" />
+          </svg>
+        ),
+        name: "coding sessions",
+        features: [
+          "excercises",
+          "Free consultaion",
+          "development",
+        ],
+      },
+      {
+        icon: (
+          <svg
+            width="28"
+            height="24"
+            viewBox="0 0 28 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M3.11111 18L0 1.5L8.55556 9L14 0L19.4444 9L28 1.5L24.8889 18H3.11111ZM24.8889 22.5C24.8889 23.4 24.2667 24 23.3333 24H4.66667C3.73333 24 3.11111 23.4 3.11111 22.5V21H24.8889V22.5Z" />
+          </svg>
+        ),
+        name: "workshops",
+        features: [
+          "excercises",
+          "Free consultaion",
+          "development",
+        ],
+      },
+  ];
+  
